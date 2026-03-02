c0900d2375eaaf7257ac6300ecda0a868b92ac0e
thelastcat15 <smart000firstfenix@gmail.com>
2026-03-02 09:03:00 +0700

    test

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,12 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"jodhere.backend/handlers"
+	"jodhere.backend/middleware"
 )
 
 func SetupRoutes(app *fiber.App) {
 	app.Get("/health", handlers.HealthCheck)
-	api := app.Group("/api/v1") // , middleware.RequireAuth
+	api := app.Group("/api/v1", middleware.RequireAuth)
 	api.Get("/", handlers.Welcome)
 
 	RegisterProfileRoutes(api.Group("/profile"))
